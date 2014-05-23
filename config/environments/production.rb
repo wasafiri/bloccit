813c3d0eca3d8e8bc3d6948e2e167267da605a00
David Bahar <dbahar@gwmail.gwu.edu>
2014-05-23 15:46:44 -0400

    Configured app for production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # http://bloccit.herokuapp.com
+  config.action_mailer.default_url_options = { host: 'bloccit.herokuapp.com' }
+
 end
