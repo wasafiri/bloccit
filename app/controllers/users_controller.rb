0d29d80eb17125c9d7fbb038f0adc1d34338c328
David Bahar <dbahar@gwmail.gwu.edu>
2014-05-31 22:18:33 -0400

    Created Popular Posts and Users, and optimized topics#show with eager loading.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
 
+  def index
+    @users = User.top_rated.paginate(page: params[:page], per_page: 10)
+  end
+
   def update
     if current_user.update_attributes(user_params)
       flash[:notice] = "User information updated"
